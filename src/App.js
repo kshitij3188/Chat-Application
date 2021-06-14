24aa08a0a23679f8f1d951fbf8caf7b7949f9e6a
kshitij <kshitijk3188@gmail.com>
2021-06-14 20:17:34 +0530

    Add Login Form

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
-
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = '7b4c5631-7ad2-4c30-ab63-1264a9387ad6';
+
 const App = () => {
-    return (
-        <ChatEngine
-            heigth="100vh"
-            projectID="7b4c5631-7ad2-4c30-ab63-1264a9387ad6"
-            userName="kshitij3188"
-            userSecret="1234"
-            renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps }/>}
-        />
-    );
-}
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
